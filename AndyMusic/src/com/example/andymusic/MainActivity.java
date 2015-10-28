eb08171804d0b10b67e2d2c470a8825e224049e1
jwc0310 <jwc0310@163.com>
2015-10-28 17:41:38 +0800

    Add player and button function

--- AndyMusic/src/com/example/andymusic/MainActivity.java
+++ AndyMusic/src/com/example/andymusic/MainActivity.java
@@ -5,6 +5,8 @@ import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.musicplayer.andymusic.AndyMusicPlayer;
+
 import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Context;
@@ -16,12 +18,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends TabActivity implements View.OnClickListener {
 
@@ -38,19 +43,18 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
 	private ImageButton start;
 	private ListView lv1;
 	
+	private AndyMusicPlayer mPlayer;
 	//播放列表
 	private List<String> mMusicList = new ArrayList<String>();
+	//当前播放歌曲的索引
+	private int currentListItem = 0;
 	//音乐路径
 	private static final String MUSIC_PATH = new String("/storage/sdcard1/music/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainui);
-        
-        //context = getBaseContext();
-        //initTabs();
-        //initViewPager();
-        
+
         //get TabHost object
         mTabHost = getTabHost();
         mTabHost.addTab(mTabHost.newTabSpec("tab1")
@@ -65,9 +69,9 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
         		.setIndicator("Aritst")
         		.setContent(R.id.tv3)
         		);
-        
+
         mTabHost.setCurrentTab(0);
-        
+
         last = (ImageButton)findViewById(R.id.last);
         next = (ImageButton)findViewById(R.id.next);
         stop = (ImageButton)findViewById(R.id.stop);
@@ -80,7 +84,11 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
 
         lv1 = (ListView)findViewById(R.id.tv1);
         musicList();
-        
+        ListClickListener l = new ListClickListener();
+        lv1.setOnItemClickListener(l);
+
+        mPlayer = new AndyMusicPlayer();
+
         mTabHost.setOnTabChangedListener(new OnTabChangeListener(){
 
 			@Override
@@ -91,7 +99,19 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
         	
         });
     }
-    
+    //监听类
+    class ListClickListener implements OnItemClickListener{
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			// TODO Auto-generated method stub
+			String text = lv1.getItemAtPosition(position).toString();
+			Log.i("Andy", "Song name is "+text);
+			currentListItem = position;
+			mPlayer.playMusic(MUSIC_PATH+mMusicList.get(currentListItem));
+		}
+    }
     //播放列表
     public void musicList(){
     	//取得指定位置的文件，设置显示到播放列表
@@ -119,16 +139,27 @@ public class MainActivity extends TabActivity implements View.OnClickListener {
 		switch(v.getId()){
 		case R.id.last:
 			Log.i("Andy", "Last button is clicked !");
+			if(--currentListItem < 0){
+				currentListItem = 0;
+				Toast.makeText(this, "this is the first song", Toast.LENGTH_SHORT).show();
+			}
+			mPlayer.playMusic(MUSIC_PATH+mMusicList.get(currentListItem));
 			break;
 		case R.id.stop:
 			Log.i("Andy", "Stop button is clicked !");
+			mPlayer.stopMusic();
 			break;
 		case R.id.start:
 			Log.i("Andy", "Start button is clicked !");
-			Log.i("Andy", "Start button is clicked 2 !");
+			mPlayer.pauseMusic();
 			break;
 		case R.id.next:
 			Log.i("Andy", "Next button is clicked !");
+			if(++currentListItem >mMusicList.size()-1){
+				currentListItem = mMusicList.size()-1;
+				Toast.makeText(this, "this is the last song", Toast.LENGTH_SHORT).show();
+			}
+			mPlayer.playMusic(MUSIC_PATH+mMusicList.get(currentListItem));
 			break;
 		}
 	}
